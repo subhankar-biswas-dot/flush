ff4429748f91175487b21e0b5b595fe4ab038506
subhankar <sub.nitt@yahoo.com>
2020-12-08 10:58:48 +0530

    first commit

---
+++ Test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=0;i<1;i++)
+    {
+
+        cout<<i<<endl;
+    }
+    cout<<"Hello World"<<endl;
+    cout<<"Hello all"<<endl;
+    return 0;
+
+
+}
